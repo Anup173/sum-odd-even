1bad790c4fc0920dc6ff2ab5e4187385cc6bfffe
Anup173 <anupdas17121997@gmail.com>
2020-05-16 15:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def result():
     u_input = request.form.get("num")
     if not u_input:
         return redirect("/")
-    number = fb(int(u_input))
-    return render_template("result.html", fib_num = number, usr_input = u_input)
+    number = 19
+    return render_template("result.html", fib_num = number)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
