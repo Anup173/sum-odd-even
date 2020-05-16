dd5be00d2a48d33afcf2c6e457244659275cbe4d
Anup173 <anupdas17121997@gmail.com>
2020-05-16 16:00:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def result():
     u_input = request.form.get("num")
     if not u_input:
         return redirect("/")
-    number = 19
+    number = 53
     return render_template("result.html", fib_num = number)
 
 if __name__ == '__main__':
